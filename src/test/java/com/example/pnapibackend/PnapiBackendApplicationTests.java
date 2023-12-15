541c8cfecb00a199a28e06e775b76250d36eb248
jules <julesvanin@gmail.com>
2023-12-15 15:52:58 +0100

    [init] initial commit

---
+++ src/test/java/com/example/pnapibackend/PnapiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pnapibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PnapiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
