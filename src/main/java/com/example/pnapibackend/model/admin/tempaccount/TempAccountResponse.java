b692f8ab446fd727e6a9e8cdc8e0601b1ad26d69
thibault <thibault.henrion@gmail.com>
2024-01-31 13:54:39 +0100

    add expiration and usage to get temporary accounts

--- src/main/java/com/example/pnapibackend/model/admin/tempaccount/TempAccountResponse.java
+++ src/main/java/com/example/pnapibackend/model/admin/tempaccount/TempAccountResponse.java
@@ -3,15 +3,19 @@ package com.example.pnapibackend.model.admin.tempaccount;
 import com.example.pnapibackend.data.entities.Role;
 import com.example.pnapibackend.data.entities.TemporaryAccount;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 
-public record TempAccountResponse(String email, String name, String countryCode, int authCode, Collection<Role> role) {
+public record TempAccountResponse(String email, String name, String countryCode, int authCode,
+                                  LocalDateTime expiration, int usage, Collection<Role> role) {
     public static TempAccountResponse getFromTempAccount(TemporaryAccount account){
         return new TempAccountResponse(
                 account.getEmail(),
                 account.getName(),
                 account.getCountryCode(),
                 account.getAuthCode(),
+                account.getExpiration(),
+                account.getUsage(),
                 account.getRoles()
         );
     }
