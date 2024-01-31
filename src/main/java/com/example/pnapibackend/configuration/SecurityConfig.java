453495b7646a95a4537892a943e0eb452250f1f2
thibault <thibault.henrion@gmail.com>
2024-01-31 13:38:46 +0100

    fix CORS (this is the patch of the year)

--- src/main/java/com/example/pnapibackend/configuration/SecurityConfig.java
+++ src/main/java/com/example/pnapibackend/configuration/SecurityConfig.java
@@ -8,9 +8,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -56,17 +58,19 @@ public class SecurityConfig{
                                 ).permitAll()
                                 .requestMatchers("/api/admin/*").hasRole("ADMIN")
                                 .requestMatchers("/api/app/*").authenticated()
-                ).csrf((csrf) ->
-                csrf.ignoringRequestMatchers("api/app/*", "api/admin/*")
-        );
+                ).cors(Customizer.withDefaults())
+                .csrf((csrf) ->
+                csrf.ignoringRequestMatchers("api/app/*", "api/admin/*"));
         return http.build();
     }
 
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS"));
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
